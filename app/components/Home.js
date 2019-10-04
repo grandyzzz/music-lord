c1d474a94327adffbedfde6f71821083c9d12de7
grandyzzz <twieank@gmail.com>
2019-10-04 18:08:22 +0200

    init file reading

--- app/components/Home.js
+++ app/components/Home.js
@@ -1,20 +1,56 @@
-// @flow
-import React, { Component } from 'react';
-import { Link } from 'react-router-dom';
-import routes from '../constants/routes';
-import styles from './Home.css';
+import React from 'react';
+import styled from 'styled-components';
 
-type Props = {};
+const Container = styled.main`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 100%;
+  max-width: 1200px;
+  margin: 0 auto;
+  min-height: 100vh;
+`;
 
-export default class Home extends Component<Props> {
-  props: Props;
+const Button = styled.button`
+  border: none;
+  background: #333;
+  outline: none;
+  border-radius: 3px;
+  color: #fff;
+  margin: 0;
+  padding: 20px;
+  font-family: 'Source Sans Pro', sans-serif;
+  font-weight: 900;
+  font-size: 50px;
+  text-transform: lowercase;
+  cursor: pointer;
+`;
 
-  render() {
-    return (
-      <div className={styles.container} data-tid="container">
-        <h2>Home</h2>
-        <Link to={routes.COUNTER}>to Counter</Link>
-      </div>
+const Home = () => {
+  function handleClick() {
+    const { remote } = require('electron');
+    const { dialog } = remote;
+    const fs = remote.require('fs');
+    dialog.showOpenDialog(
+      {
+        properties: ['openFile', 'openDirectory', 'multiSelections']
+      },
+      result => {
+        console.log(result);
+        fs.readdir(result[0], (err, files) => {
+          fs.readFile(result[0] + '/' + files[0], (err, data) => {
+            console.log(data);
+          })
+        });
+      }
     );
   }
-}
+
+  return (
+    <Container>
+      <Button onClick={handleClick}>заебалб выбери папку</Button>
+    </Container>
+  );
+};
+
+export default Home;
